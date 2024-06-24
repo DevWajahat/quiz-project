689181b1d0f49f60261f75259364dc54f3e4d095
Wajahat Abbas <135859873+panotiwajhat@users.noreply.github.com>
2024-06-24 11:34:09 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// function handleClick(event) {
+//     event.preventDefault(); // Prevent form submission
+
+//     var email = document.getElementById("email").value;
+//     var password = document.getElementById("psw").value;
+//     var name = document.getElementById("name").value;
+
+//     // Store data in local storage
+//     localStorage.setItem("email", email);
+//     localStorage.setItem("password", password);
+//     localStorage.setItem("name", name);
+
+//     // Redirect to login.html
+//     window.location.href = "login.html";
+// }
